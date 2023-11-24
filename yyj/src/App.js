6229b9e1ec28bc19ea63fc9a3958d615a485748e
yeahzzii <107247219+yeahzzii@users.noreply.github.com>
2023-11-24 16:24:38 +0900

    chapter 6 Recap 파트까지

--- yyj/src/App.js
+++ yyj/src/App.js
@@ -1,11 +1,30 @@
-import Button from "./Button";
-import styles from "./App.module.css";
+import { useState, useEffect } from "react";
 
 function App() {
+  const [counter, setValue] = useState(0);
+  const [keyword, setKeyword] = useState("");
+  const onClick = () => setValue((prev) => prev +1);
+  const onChange = (event) => setKeyword(event.target.value);
+  useEffect(() => {
+    console.log("I run only once");
+  }, []);
+  useEffect(() => {
+    if(keyword !== "" && keyword.length > 5) { // 키워드가 비어있을 땐 실행하지 않음
+      console.log("I run when 'keyword' changes");
+    }
+  }, [keyword]); // 키워드가 변화할 때 실행할 것임
+  useEffect(() => {
+    console.log("I run when 'counter' changes");
+  }, [counter]); 
+  useEffect(() => {
+    console.log("I run when keyword & counter change");
+  }, [keyword, counter]);
+
   return (
     <div>
-      <h1 className={styles.title}>Welcome Back!</h1>
-      <Button text={"Continue"} />
+      <input value={keyword} onChange={onChange} type="text" placeholder="Search here..." />
+      <h1>{counter}</h1>
+      <button onClick={onClick}>click me</button>
     </div>
   );
 }
