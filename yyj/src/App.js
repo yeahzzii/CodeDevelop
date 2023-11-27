59be3165ec01443f89f97a7a9fbc36fe794b8d5b
yeahzzii <107247219+yeahzzii@users.noreply.github.com>
2023-11-27 16:30:01 +0900

    chapter 7 퍼블 전

--- yyj/src/App.js
+++ yyj/src/App.js
@@ -1,28 +1,26 @@
-import { useEffect, useState } from "react";
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+} from "react-router-dom";
+import Home from "./routes/Home";
+import Detail from "./routes/Detail";
 
 function App() {
-  const [loading, setLoading] = useState(true);
-  const [coins, setCoins] = useState([]);
-  useEffect(() => {
-    fetch("https://api.coinpaprika.com/v1/tickers")
-    .then((response) => response.json())
-    .then((json) => {
-      setCoins(json);
-      setLoading(false);
-    });
-  }, [])
   return (
-    <div>
-      <h1>The Coins! {loading ? "" : `(${coins.length})`} </h1>
-      {loading ? (<strong>Loading...</strong>) : (
-        <select>
-          {coins.map((coin) => 
-            <option>
-              {coin.name} ({coin.symbol}) : ${coin.quotes.USD.price} USD
-            </option>)}
-        </select>)}
-    </div>
-  )
+  <Router>
+    <Routes>
+      <Route path="/hello" element = {<h1>Hello</h1>} >
+
+      </Route>
+      <Route path="/movie/:id" element = {<Detail />}>
+
+      </Route>
+      <Route path="/" element = {<Home />}>
+
+      </Route>
+    </Routes>
+  </Router>);
 }
 
 export default App;
