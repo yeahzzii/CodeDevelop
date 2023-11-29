5fb1040fff15e0f6bc6a4cd1efd4c0f9a2b209e9
yeahzzii <107247219+yeahzzii@users.noreply.github.com>
2023-11-29 16:09:56 +0900

    노마드코더 영화 웹서비스 만들기 finish

--- yyj/src/routes/Detail.js
+++ yyj/src/routes/Detail.js
@@ -1,19 +1,52 @@
-import { useEffect } from "react";
-import { useParams } from "react-router-dom";
+import PropTypes from "prop-types";
+import React, { useState, useEffect } from "react";
+import { useParams} from "react-router-dom";
+import styles from "./Detail.module.css";
 
 function Detail() {
-    const {id} = useParams(); // url로 받은 파라미터값을 반환한다.
-    const getMovie = async() => {
-        const json = await (await fetch(`https://yts.mx/api/v2/movie_details.json?movie_id=${id}`)).json();
-        console.log(json);
-    }
+    const {id} = useParams();
+    const [movie, setMovie] = useState(null);
+
     useEffect(() => {
+        const getMovie = async () => {
+            try {
+                const response = await fetch(`https://yts.mx/api/v2/movie_details.json?movie_id=${id}`);
+                const json = await response.json();
+                console.log(json);
+                setMovie(json.data.movie);
+            } catch (error) {
+                console.error("Error fetching movie details:", error);
+            }
+        };
+
         getMovie();
-    }, []);     
-    console.log({id});
+    }, [id]);
+
+    // movie가 null이면 아무것도 반환하지 않음
+    if (!movie) {
+        return null;
+    }
+
+    // movie가 null이 아닐 때만 아래 내용을 반환
     return (
-        <h1>Detail</h1>
-    ); 
+        <div className={styles.detail}>
+            <img src={movie.medium_cover_image} alt={movie.title} className={styles.movie__img} />
+            <h2 className={styles.movie__title}>{movie.title}</h2>
+            <h3 className={styles.movie__year}>{movie.year}</h3>
+            <p>{movie.description_full.length > 235 ? `${movie.description_full.slice(0, 235)}`: movie.description_full}</p>
+            <ul className={styles.movie__genres}>
+                {movie.genres.map(g => (<li key={g}>{g}</li>))}
+            </ul>
+        </div>
+    );
+}
+
+Detail.propTypes = {
+    id: PropTypes.number.isRequired,
+    medium_cover_image: PropTypes.string.isRequired,
+    title: PropTypes.string.isRequired,
+    summary: PropTypes.string.isRequired,
+    genres: PropTypes.arrayOf(PropTypes.string.isRequired)
 }
 
 export default Detail;
--- yyj/src/routes/Home.js
+++ yyj/src/routes/Home.js
@@ -1,5 +1,6 @@
 import { useState, useEffect } from "react";
 import Movie from "../components/Movies"
+import styles from "./Home.module.css";
 
 function Home() {
     const [loading, setLoading] = useState(true);
@@ -14,13 +15,14 @@ function Home() {
     }, []);
     console.log(movies);
     return (
-        <div>
+        <div className={styles.container}>
         {loading ? (<h1>Loading...</h1>) : (
-            <div>
+            <div className={styles.movies}>
             {movies.map(movie => (
                 <Movie 
                 key={movie.id}
                 id={movie.id}
+                year={movie.year}
                 coverImg={movie.medium_cover_image} 
                 title={movie.title} 
                 summary={movie.summary} 
